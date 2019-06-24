723a62ee5e98b60aa71f7ae34c62c97d8fcc29c3
naufalafif <nafualafif58@gmail.com>
2019-06-24 10:20:24 +0700

    change dplk configuration layout

--- src/router/modules/dplk-configuration.js
+++ src/router/modules/dplk-configuration.js
@@ -14,38 +14,47 @@ export default {
     {
       path: 'dplk-information',
       name: 'DplkInformation',
-      component: () => import('@/views/dplk-configuration/dplk-information/index'),
+      component: () => import('@/views/dplk-configuration/index'),
       meta: {
-        title: 'dplkInformation',
-        roles: ['admin'] // or you can only set roles in sub nav
-      }
-    },
-    {
-      path: 'dplk-address',
-      name: 'DplkAddress',
-      component: () => import('@/views/dplk-configuration/dplk-address/index'),
-      meta: {
-        title: 'dplkAddress',
-        roles: ['admin'] // or you can only set roles in sub nav
-      }
-    },
-    {
-      path: 'dplk-bank-account',
-      name: 'DplkBankAccount',
-      component: () => import('@/views/dplk-configuration/dplk-bank-account/index'),
-      meta: {
-        title: 'dplkBankAccount',
-        roles: ['admin'] // or you can only set roles in sub nav
-      }
-    },
-    {
-      path: 'dplk-staff',
-      name: 'DplkStaff',
-      component: () => import('@/views/dplk-configuration/dplk-staff/index'),
-      meta: {
-        title: 'dplkStaff',
+        title: 'dplkConfiguration',
         roles: ['admin'] // or you can only set roles in sub nav
       }
     }
+    // {
+    //   path: 'dplk-information',
+    //   name: 'DplkInformation',
+    //   component: () => import('@/views/dplk-configuration/dplk-information/index'),
+    //   meta: {
+    //     title: 'dplkInformation',
+    //     roles: ['admin'] // or you can only set roles in sub nav
+    //   }
+    // },
+    // {
+    //   path: 'dplk-address',
+    //   name: 'DplkAddress',
+    //   component: () => import('@/views/dplk-configuration/dplk-address/index'),
+    //   meta: {
+    //     title: 'dplkAddress',
+    //     roles: ['admin'] // or you can only set roles in sub nav
+    //   }
+    // },
+    // {
+    //   path: 'dplk-bank-account',
+    //   name: 'DplkBankAccount',
+    //   component: () => import('@/views/dplk-configuration/dplk-bank-account/index'),
+    //   meta: {
+    //     title: 'dplkBankAccount',
+    //     roles: ['admin'] // or you can only set roles in sub nav
+    //   }
+    // },
+    // {
+    //   path: 'dplk-staff',
+    //   name: 'DplkStaff',
+    //   component: () => import('@/views/dplk-configuration/dplk-staff/index'),
+    //   meta: {
+    //     title: 'dplkStaff',
+    //     roles: ['admin'] // or you can only set roles in sub nav
+    //   }
+    // }
   ]
 }
---
+++ src/views/dplk-configuration/index.vue
@@ -0,0 +1,46 @@
+<template lang="pug">
+.tab-container
+  el-tabs.pensiun-tab(type='border-card')
+    el-tab-pane(:label='$t("route.dplkInformation")')
+      Information
+    el-tab-pane(:label='$t("route.dplkStaff")')
+      Staff
+    el-tab-pane(:label='$t("route.dplkAddress")')
+      Address
+    el-tab-pane(:label='$t("route.dplkBankAccount")')
+      Bank
+</template>
+
+<script>
+import Information from './components/dplk-information'
+import Staff from './components/dplk-staff'
+import Address from './components/dplk-address'
+import Bank from './components/dplk-bank-account'
+
+export default {
+  name: 'Location',
+  components: {
+    Information,
+    Staff,
+    Address,
+    Bank
+  },
+  data() {
+    return {
+      updated: undefined
+    }
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+  .tab-container {
+    margin: 30px;
+  }
+  .pensiun-tab {
+    margin-top: 15px;
+  }
+</style>
