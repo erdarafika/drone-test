f13d53da0dad54b7a6a0677c62d12eeaef2266c6
Naufal afif <naufalafif58@gmail.com>
2019-07-16 11:50:30 +0700

    enable route saver on state

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,7 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   language: state => state.app.language,
+  route: state => state.user.currentRoute,
   size: state => state.app.size,
   device: state => state.app.device,
   visitedViews: state => state.tagsView.visitedViews,
