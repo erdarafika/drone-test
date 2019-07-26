97dd035d32f1f5b2493a0bef66d4b58a2781d6f9
Naufal Afif <naufalafif58@gmail.com>
2019-07-26 14:57:45 +0700

    remove unused component

--- src/views/dashboard/admin/index.vue
+++ src/views/dashboard/admin/index.vue
@@ -15,47 +15,13 @@
       </el-col>
     </el-row>
 
-    <!-- <el-row :gutter="32">
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <raddar-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <pie-chart />
-        </div>
-      </el-col>
-      <el-col :xs="24" :sm="24" :lg="8">
-        <div class="chart-wrapper">
-          <bar-chart />
-        </div>
-      </el-col>
-    </el-row> -->
-
-    <!-- <el-row :gutter="8">
-      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
-        <transaction-table />
-      </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 12}" :xl="{span: 12}" style="margin-bottom:30px;">
-        <todo-list />
-      </el-col>
-      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
-        <box-card />
-      </el-col>
-    </el-row> -->
   </div>
 </template>
 
 <script>
 import PanelGroup from './components/PanelGroup'
 import LineChart from './components/LineChart'
-// import RaddarChart from './components/RaddarChart'
-// import PieChart from './components/PieChart'
-// import BarChart from './components/BarChart'
-// import TransactionTable from './components/TransactionTable'
 import TodoList from './components/TodoList'
-// import BoxCard from './components/BoxCard'
 
 const lineChartData = {
   fundData: {
@@ -69,12 +35,7 @@ export default {
   components: {
     PanelGroup,
     LineChart,
-    // RaddarChart,
-    // PieChart,
-    // BarChart,
-    // TransactionTable,
     TodoList
-    // BoxCard
   },
   data() {
     return {
@@ -92,8 +53,6 @@ export default {
 <style lang="scss" scoped>
 .dashboard-editor-container {
   padding: 32px;
-  // background-color: rgb(240, 242, 245);
-  // PensiunKu version
   background-color: #e4e4e4;
 
   position: relative;
