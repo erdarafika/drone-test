aba80a3e3f78f4816e244c9e64e9d0903b89c2fd
Naufal Afif <naufalafif58@gmail.com>
2019-07-30 15:08:04 +0700

    add new validator

--- src/views/client-administration/company/validation-rules.js
+++ src/views/client-administration/company/validation-rules.js
@@ -1,18 +1,19 @@
-import { requiredValidator, emailValidator, numberValidator } from '@/global-function/formValidator'
+import { requiredValidator, emailValidator, numberValidator, alphabeticValidator, alphanumericLineDotSlashValidator } from '@/global-function/formValidator'
 
 export default {
-  name: [requiredValidator],
+  name: [requiredValidator, alphabeticValidator],
   code: [],
   email: [requiredValidator, emailValidator],
   website: [requiredValidator],
   businessLineId: [requiredValidator],
   businessEntity: [requiredValidator],
   npwp: [requiredValidator],
-  deedEstablishmentNumber: [requiredValidator],
-  articleAssociationNumber: [],
-  latestAmendmentArticleAssociationNumber: [],
-  companyNumberRegistrationNumber: [],
-  domicilieCertificateNumber: [],
+
+  deedEstablishmentNumber: [requiredValidator, alphanumericLineDotSlashValidator],
+  articleAssociationNumber: [alphanumericLineDotSlashValidator],
+  latestAmendmentArticleAssociationNumber: [alphanumericLineDotSlashValidator],
+  companyNumberRegistrationNumber: [alphanumericLineDotSlashValidator],
+  domicilieCertificateNumber: [alphanumericLineDotSlashValidator],
 
   domicilieCertificateNumberExpiredDate: [],
   deedEstablishmentDate: [requiredValidator],
@@ -23,7 +24,7 @@ export default {
   asset: [requiredValidator],
   grossIncomePerYear: [requiredValidator],
   pensionProgramSubmissionPurpose: [requiredValidator],
-  moneySource: [requiredValidator],
+  moneySource: [requiredValidator, alphabeticValidator],
 
   office: [numberValidator],
   office2: [numberValidator],
