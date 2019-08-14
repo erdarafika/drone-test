7815cc28588cb81eae207b2a586d7fc26cd15cf4
Alvinditya Saputra <alvindityas@gmail.com>
2019-08-14 21:44:20 +0700

    add module add billing ppip organization

--- src/components/UploadExcel/index.vue
+++ src/components/UploadExcel/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <input ref="excel-upload-input" class="excel-upload-input" type="file" accept=".xlsx, .xls" @change="handleClick">
-    <div class="drop" @drop="handleDrop" @dragover="handleDragover" @dragenter="handleDragover">
+    <div class="drop" style="max-height: 50px; line-height: 45px;" @drop="handleDrop" @dragover="handleDragover" @dragenter="handleDragover">
       Drop excel file here or
       <el-button :loading="loading" style="margin-left:16px;" size="mini" type="primary" @click="handleUpload">
         Browse
--- src/views/contribution/billing-ppip-organization/index.vue
+++ src/views/contribution/billing-ppip-organization/index.vue
@@ -5,32 +5,41 @@ app-container
       el-row(:gutter='40')
         el-col(:span='12')
           el-form(ref='dataForm', :rules='rules', :model='temp', label-position='left', label-width='150px', style='width: 80%')
-            el-form-item(:label="`Company`", prop='companyId')
+            el-form-item(:label="$t('billing.companyId')", prop='companyId')
               el-select(v-model='temp.companyId', name='company' placeholder='Select', filterable, default-first-option)
                 el-option(v-for='item in companyOptions', :key='item.value', :label='item.label', :value='item.value')
-            el-form-item(:label="`Group`", prop='groupId')
+            el-form-item(:label="$t('billing.groupId')", prop='groupId')
               el-select(v-model='temp.groupId', name='group' placeholder='Select', filterable, default-first-option)
                 el-option(v-for='item in groupOptions', :key='item.value', :label='item.label', :value='item.value')
-            el-form-item(:label="`Effective Date`", prop='effectiveDate' )
-              el-date-picker(:value-format='dateFormat' v-model='temp.effectiveDate', type='date', placeholder='Pick a day' name='date')
-            el-form-item(:label="`Import Billing`", prop='billing')
-              upload-excel-component(:on-success='handleSuccess', :before-upload='beforeUpload' style='width: 100%;')
+            el-form-item(:label="$t('billing.billingDate')", prop='billingDate' )
+              el-date-picker(:value-format='dateFormat' v-model='temp.billingDate', type='date', placeholder='Pick a day' name='date')
+            el-form-item(:label="$t('billing.importBilling')", prop='file')
+              upload-excel-component(:on-success='handleSuccess()', :before-upload='beforeUpload' style='width: 100%;' ref="file")
+              span(class='el-alert el-alert--success' v-if='temp.file !== undefined') {{ temp.file.name }}
             el-form-item
-              RequestApproval.pull-right(:callback='requestApproval')
-              el-button.pull-right(size='small' @click='resetTemp') Cancel
-        el-col(:span='12')
-          h4(style='color:#646266') Billing Data Preview
-          el-table(:data='tableData', border='', highlight-current-row='')
-            el-table-column(v-for='item of tableHeader', :key='item', :prop='item', :label='item')
-
+              table.pull-right
+                td
+                  el-button.pull-right(size='small' @click='resetTemp') Cancel
+                td
+                  RequestApproval.pull-right(:callback='requestApproval')
+        el-col(:span='12' v-if='isConflict')
+          h4(style='color:#646266') Invalid Data
+          el-table(:data='errorsData')
+            el-table-column(:label="$t('table.lineNumber')")
+              template(slot-scope='scope')
+                span {{ scope.row.line }}
+            el-table-column(:label="$t('table.errors')")
+              template(slot-scope='scope')
+                span(v-for="item in scope.row.errors") <strong>{{ item.field }}</strong> {{ item.message }}<br/>
 </template>
 
 <script>
-import { createRecord, approveRecord } from '@/api/static/contribution-billing-ppip-organization'
+import { upload } from '@/api/contribution-billing'
 import { fetchList as fetchCompany } from '@/api/company'
 import { fetchList as fetchGroup } from '@/api/group-maintenance'
 import rules from './validation-rules'
 import UploadExcelComponent from '@/components/UploadExcel/index.vue'
+import { Notification } from 'element-ui'
 
 export default {
   components: { UploadExcelComponent },
@@ -42,13 +51,28 @@ export default {
       listLoading: true,
       companyOptions: [],
       groupOptions: [],
+      isConflict: true,
+      errorsData: [],
       temp: {
         companyId: undefined,
         groupId: undefined,
-        effectiveDate: undefined,
-        billing: undefined
+        billingDate: undefined,
+        file: undefined
       },
       rules
+    }
+  },
+  watch: {
+    'temp.companyId': function(companyId) {
+      if (!this.initialUpdate) {
+        this.temp.groupId = undefined
+      }
+      if (companyId) {
+        fetchGroup({ companyId: companyId }).then(res => {
+          res = res.filter(item => item.status === 'active' && item.productType.code === 'dplk' && item.type === 'organization')
+          this.groupOptions = res.map(item => ({ value: item.id, label: item.name }))
+        })
+      }
     }
   },
   created() {
@@ -57,16 +81,12 @@ export default {
       res = res.filter(item => item.status === 'active')
       this.companyOptions = res.map(item => ({ value: item.id, label: item.name }))
     })
-    fetchGroup().then(res => {
-      res = res.filter(item => item.status === 'active')
-      this.groupOptions = res.map(item => ({ value: item.id, label: item.name }))
-    })
   },
   methods: {
     beforeUpload(file) {
       const isLt1M = file.size / 1024 / 1024 < 1
-
       if (isLt1M) {
+        this.temp.file = file
         return true
       }
 
@@ -76,27 +96,55 @@ export default {
       })
       return false
     },
-    handleSuccess({ results, header }) {
-      this.temp.billing = { header, results }
-      this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate('billing')
-      })
-
-      this.tableData = results
-      this.tableHeader = header
+    handleSuccess() {
+      if (this.temp.file !== undefined) {
+        const duration = 3500
+        Notification({
+          message: 'File added: ' + this.temp.file.name,
+          type: 'success',
+          duration
+        })
+      }
     },
     requestApproval() {
+      this.temp.billingType = 'dplk'
       this.$refs['dataForm'].validate((valid) => {
         if (valid) {
-          createRecord(this.temp).then((response) => {
+          const formData = new FormData()
+          formData.append('groupId', this.temp.groupId)
+          formData.append('billingDate', this.temp.billingDate)
+          formData.append('file', this.temp.file)
+          upload(formData).then((response) => {
             if (response.status_code >= 200 && response.status_code <= 300) {
-              approveRecord(this.temp).then(response => {
-                if (response.status_code >= 200 && response.status_code <= 300) {
-                  this.successNotifier()
-                }
-                this.$router.push({ name: 'ContributionBilling' })
-              })
+              this.successNotifier()
+              this.back()
             }
+          }).catch((err) => {
+            if (err.response.data.error === 'conflict') {
+              const errors = err.response.data.errors
+              let lineNumber = 1
+              const listErrors = []
+              errors.forEach(function(data) {
+                const listError = []
+                data.forEach(function(error) {
+                  const errorMessage = {
+                    field: error.field,
+                    message: error.defaultMessage
+                  }
+                  listError.push(errorMessage)
+                })
+                const constructData = {
+                  line: lineNumber++,
+                  errors: listError
+                }
+                listErrors.push(constructData)
+              })
+              console.log(listErrors)
+              this.errorsData = listErrors
+              this.temp.file = undefined
+              this.isConflict = true
+            }
+            // this.errorsData = err.response.data.errors
           })
         }
       })
@@ -105,9 +153,10 @@ export default {
       this.temp = {
         companyId: undefined,
         groupId: undefined,
-        effectiveDate: undefined,
-        billing: undefined
+        billingDate: undefined,
+        file: undefined
       }
+      this.isConflict = false
       this.$nextTick(() => {
         this.$refs['dataForm'].clearValidate()
       })
